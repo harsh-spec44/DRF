497cedc864ebfacecf423f61fc985486a69539f4
harsh-spec44 <289032530+harsh-spec44@users.noreply.github.com>
2026-08-22 01:01:58 +0530

    Serializers

--- peoplehub/api/views.py
+++ peoplehub/api/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from .models import Person
 from .serializers import PersonSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+
 # Create your views here.
 
 def singleobj(request):
     data = Person.objects.get(id=1)
     serilaizer = PersonSerializer(data)
-    print(serilaizer.data)
+    json_data = JSONRenderer().render(serilaizer.data)
+    return HttpResponse(json_data, 
+                        content_type='application/json')
 
 def multiobj(request):
     data = Person.objects.all()
     serilaizer = PersonSerializer(data,many=True)
-    print(serilaizer.data)
+    json_data = JSONRenderer().render(serilaizer.data)
+    return HttpResponse(json_data, 
+                        content_type='application/json')
