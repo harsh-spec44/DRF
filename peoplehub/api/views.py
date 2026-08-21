4e651cd6095751b86e54af2b9b4dc20b14f25544
harsh-spec44 <289032530+harsh-spec44@users.noreply.github.com>
2026-08-22 00:41:25 +0530

    Serialization Process

--- peoplehub/api/views.py
+++ peoplehub/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Person
+from .serializers import PersonSerializer
 # Create your views here.
+
+def singleobj(request):
+    data = Person.objects.get(id=1)
+    serilaizer = PersonSerializer(data)
+    print(serilaizer.data)
+
+def multiobj(request):
+    data = Person.objects.get(id=1)
+    serilaizer = PersonSerializer(data,many=True)
+    print(serilaizer.data)
