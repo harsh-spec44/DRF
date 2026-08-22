b24751c7bad398d21054e192472095057714db11
harsh-spec44 <289032530+harsh-spec44@users.noreply.github.com>
2026-08-22 22:46:45 +0530

    Parsing Request data with JSONParser

--- peoplehub/api/views.py
+++ peoplehub/api/views.py
@@ -3,7 +3,9 @@ from .models import Person
 from .serializers import PersonSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
-
+import io
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def singleobj(request):
@@ -13,7 +15,13 @@ def singleobj(request):
     return HttpResponse(json_data, 
                         content_type='application/json')
 
+@csrf_exempt
 def multiobj(request):
+    if request.method == "POST":
+        json = request.body
+        stream = io.BytesIO(json)
+        parsed_data = JSONParser().parse(stream)
+        print(parsed_data)
     data = Person.objects.all()
     serilaizer = PersonSerializer(data,many=True)
     json_data = JSONRenderer().render(serilaizer.data)
