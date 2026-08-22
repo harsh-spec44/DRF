a90799c6581974d9749229efc2369c65aee91e55
harsh-spec44 <289032530+harsh-spec44@users.noreply.github.com>
2026-08-22 23:22:34 +0530

    Implementing the Create Method

--- peoplehub/api/views.py
+++ peoplehub/api/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from .models import Person
 from .serializers import PersonSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import io
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from  rest_framework import status
+
 # Create your views here.
 
 def singleobj(request):
+    
     data = Person.objects.get(id=1)
     serilaizer = PersonSerializer(data)
     json_data = JSONRenderer().render(serilaizer.data)
@@ -17,11 +20,18 @@ def singleobj(request):
 
 @csrf_exempt
 def multiobj(request):
+
     if request.method == "POST":
         json = request.body
         stream = io.BytesIO(json)
         parsed_data = JSONParser().parse(stream)
-        print(parsed_data)
+        serilaizer = PersonSerializer(data = parsed_data)
+        if serilaizer.is_valid():
+            serilaizer.save()
+            return JsonResponse({"create":"successfull"}, status=status.HTTP_201_CREATED)
+
+        return JsonResponse(serilaizer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     data = Person.objects.all()
     serilaizer = PersonSerializer(data,many=True)
     json_data = JSONRenderer().render(serilaizer.data)
