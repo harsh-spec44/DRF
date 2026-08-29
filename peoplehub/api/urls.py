17b7dd9a7f0ebc862cdf8217d81479a2ec80ec20
harsh-spec44 <289032530+harsh-spec44@users.noreply.github.com>
2026-08-29 23:05:44 +0530

    Generic API Views & Mixins

--- peoplehub/api/urls.py
+++ peoplehub/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('singleobj/<int:id>/',views.SingleObjAPIView.as_view()),
+    path('singleobj/<int:pk>/',views.SingleObjAPIView.as_view()),
     path('multiobj/',views.MultipleObjAPIView.as_view()),
 
 ]
--- peoplehub/api/views.py
+++ peoplehub/api/views.py
@@ -10,6 +10,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import CreateModelMixin,DestroyModelMixin,UpdateModelMixin,ListModelMixin,RetrieveModelMixin
 # Create your views here.
 
 @api_view(['GET','PUT','PATCH'])
@@ -54,42 +56,34 @@ def multiobj(request):
      serilaizer = PersonModelSerializer(data,many=True)
      return Response(serilaizer.data)
 
-class MultipleObjAPIView(APIView):
-   
-   def get(self, request):
-     data = Person.objects.all()
-     serilaizer = PersonModelSerializer(data,many=True)
-     return Response(serilaizer.data)
+class MultipleObjAPIView(CreateModelMixin,ListModelMixin, GenericAPIView):
+   queryset = Person.objects.all()
+   serializer_class = PersonModelSerializer
+
+# args-unnamed parameters as pupil
+# kwargs-named parameters as dictionary
+   def get(self, request,*args, **kwargs):
+     return self.list(request, *args, **kwargs,)
    
    def post(self, request):
-      parsed_data = request.data
-      serilaizer = PersonModelSerializer(data = parsed_data)
-      serilaizer.is_valid(raise_exception=True)
-      serilaizer.save()
-      return Response({"create":"successfull"}, status=status.HTTP_201_CREATED)
 
-class SingleObjAPIView(APIView):
-    def get(self, request, id):
-        data = get_object_or_404(Person,id=id)
-        serilaizer = PersonModelSerializer
-        return Response(serilaizer.data)
+     return self.create(request)
+
+class SingleObjAPIView(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView):
+    def get(self, request,*args, **kwargs):
+        
+        return self.retrieve(request,*args, **kwargs)
     
-    def put(self, request, id):
-        data = get_object_or_404(Person,id=id)
-        parsed_data = request.data
-        serilaizer = PersonModelSerializer(data,data=parsed_data)
-        if serilaizer.is_valid():
-            serilaizer.save()
-            return Response({'update':'success'})
-        return Response(serilaizer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+ 
+        return self.update(request, *args, **kwargs)
 
-    def patch(self, request, id):
-         data = get_object_or_404(Person,id=id)
-         parsed_data = request.data
-         serilaizer = PersonModelSerializer(data,data=parsed_data, partial=True)
-         if serilaizer.is_valid():
-            serilaizer.save()
-            return Response({'update':'success'})
-         return Response(serilaizer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def patch(self, request, *args, **kwargs):
+
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request,*args, **kwargs):
+       
+       return self.destroy(request, *args, **kwargs)
  
 
