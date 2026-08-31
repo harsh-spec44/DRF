f9d9c96af3ff1ab6a2d57eb61b79b879fb669883
harsh-spec44 <289032530+harsh-spec44@users.noreply.github.com>
2026-08-31 23:08:44 +0530

    User Logout Logic (Deleting Token)

--- peoplehub/user_auth/urls.py
+++ peoplehub/user_auth/urls.py
@@ -3,5 +3,7 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('login/', obtain_auth_token)
+    path('login/', obtain_auth_token),
+    path('register/', views.UserRegistration.as_view()),
+    path('logout/', views.LogoutAPIView.as_view()),
 ]
--- peoplehub/user_auth/views.py
+++ peoplehub/user_auth/views.py
@@ -4,8 +4,9 @@ from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import RegistrationUserSerializer
 # Create your views here.
-
+# GMC yukon denali ultimate - black, mercdes g63 amg - grey| blue, rr ghost - blue| brown, 12 cillindri , urus blue, bentley continental grey-black
 class LoginAPIView(APIView):
     authentication_classes = []
     permission_classes = []
@@ -21,5 +22,18 @@ class LoginAPIView(APIView):
 
             return Response({'token':token.key})
         
-
         return Response({'error': "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+class UserRegistration(APIView):
+    permission_classes = []
+    def post(self, request):
+        serializer = RegistrationUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'message':'User created successfully'}, status=status.HTTP_201_CREATED)
+
+class LogoutAPIView(APIView):
+
+    def post(self, request):
+        request.auth.delete()
+        return Response({"msg":'Logout Success'})
