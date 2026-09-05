c540a512af5ff6b99c5a4c4e7ac406a4541fb016
harsh-spec44 <289032530+harsh-spec44@users.noreply.github.com>
2026-09-05 23:03:30 +0530

    To dolist RESTAPI

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('todo/',views.TodoListCreateView.as_view()), # .as_view() :- Used when class based API View
+    path('', views.RootAPIView.as_view(), name='root'),
+    path('todo/',views.TodoListCreateView.as_view(), name='list'), # .as_view() :- Used when class based API View
     path('todo/<int:pk>/', views.RetrieveUpdateDestroyAPIView.as_view()),
 ]
--- todo/api/views.py
+++ todo/api/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Todo
 from .serializers import TodoSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 # Create your views here.
 class TodoListCreateView(ListCreateAPIView):
      queryset = Todo.objects.all()
      serializer_class = TodoSerializer
+
+     def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
 
      def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
@@ -13,3 +19,12 @@ class TodoListCreateView(ListCreateAPIView):
 class TodoRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
      queryset = Todo.objects.all()
      serializer_class= TodoSerializer
+
+     def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+class RootAPIView(APIView):
+    def get(self, request):
+        return Response({
+            'list':reverse('list', request=self.request)
+        })
